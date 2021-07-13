9eec1dc844aa8a5a6bfb18d7cf847b7fb605419d
Jeff Chiu <jwchiu82@gmail.com>
2021-07-12 20:45:21 -0700

    define check_answer

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import random
 
 # format account data into printable format
 def format_data(account):
-  """format account data into printable format """
+  """format account data and returns into printable format """
   account_name = account['name']
   account_desc = account['description']
   account_country = account['country']
   return f"{account_name}, a {account_desc}, from {account_country}"
+
+def check_answer(guess, a_followers, b_followers):
+  """use if statement to check answer and returns if correct"""
+  if a_followers > b_followers:
+    return guess == 'a'
+  else: 
+    return guess == 'b'
 
 
 #display art
@@ -25,8 +32,12 @@ print(vs)
 print(f"Against B: {format_data(account_b)}.")
 
 # ask user to guess
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
 # check if user is correct
+a_follower_count = account_a("follower_count")
+b_follower_count = account_b("Follower_count")
+
 ## get follower count of each account
 ## Use if statement to check if correct
 
