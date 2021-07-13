9d4d0ac1b249a9376c1e82e0502304e3b9c07e79
Jeff Chiu <jwchiu82@gmail.com>
2021-07-12 18:23:48 -0700

    create file structure of project

---
+++ main.py
@@ -0,0 +1,6 @@
+# break down problem: import game data; randomly select an entry from game_data list of dictionary entries and compare to another item; get user to select which one has a higher follower count.  If user choses wrong, game ends, if correct, 2nd item becomes the first item and is comapred to a new list item.
+# make a to do list: import game data, assign random list items to variables, display them in the correct manner, make fxn that replaces first list item if user is correct, figure out a way to score
+# start with the easiest todo
+# turn problem into comments
+# start coding
+# move to next task
