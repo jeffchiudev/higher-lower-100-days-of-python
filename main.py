7feb0b5315fa7d6bafb74697662e42f1a691a002
Jeff Chiu <jwchiu82@gmail.com>
2021-07-12 21:01:14 -0700

    allow the shift of account b to account a if user guesses correcctly

--- main.py
+++ main.py
@@ -21,37 +21,46 @@ def check_answer(guess, a_followers, b_followers):
 #display art
 print(logo)
 score = 0
-
-# generate random accounts from game data
-account_a = random.choice(data)
+game_should_continue = True
 account_b = random.choice(data)
-if account_a == account_b:
+
+
+# make game repeatable.
+while game_should_continue: 
+  # generate random accounts from game data
+
+  # make account at position B become next account at position A
+  account_a = account_b 
   account_b = random.choice(data)
+  
+  while account_a == account_b:
+    account_b = random.choice(data)
 
-print(f"Compare A: {format_data(account_a)}.")
-print(vs)
-print(f"Against B: {format_data(account_b)}.")
+  print(f"Compare A: {format_data(account_a)}.")
+  print(vs)
+  print(f"Against B: {format_data(account_b)}.")
 
-# ask user to guess
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+  # ask user to guess
+  guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-# check if user is correct
-## get follower count of each account
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
+  # check if user is correct
+  ## get follower count of each account
+  a_follower_count = account_a["follower_count"]
+  b_follower_count = account_b["follower_count"]
+  is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-# give user feedback
-# Score keeping
-if is_correct:
-  score += 1
-  print(f"You're right! Current score: {score}.")
-else:
-  print(f"Sorry, that's wrong. Final score: {score}.")
+  # give user feedback
+  # Score keeping
+  if is_correct:
+    score += 1
+    print(f"You're right! Current score: {score}.")
+  else:
+    game_should_continue = False
+    print(f"Sorry, that's wrong. Final score: {score}.")
 
 
 
 
-# make account at position B become next account at position A
+
 
 # game clears the screen
